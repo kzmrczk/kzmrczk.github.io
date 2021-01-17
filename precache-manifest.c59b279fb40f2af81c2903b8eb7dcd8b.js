fe58d6f6df33ff0faeb7d553b39ae7ed26fab787
kzmrczk <kazimierczak.dawid1@gmail.com>
2021-01-17 21:13:43 +0100

    Updates

---
+++ precache-manifest.c59b279fb40f2af81c2903b8eb7dcd8b.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8935d47abdb995c642bf3e13dc62a395",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f021d02a8fde673880e4",
+    "url": "/static/css/2.8935a6e8.chunk.css"
+  },
+  {
+    "revision": "32d4058f7d1b9638c4e8",
+    "url": "/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "151f6cdde99e468798b2",
+    "url": "/static/css/4.ee2a0142.chunk.css"
+  },
+  {
+    "revision": "9ca26a7d2b4a06e4f44a",
+    "url": "/static/css/main.a39178aa.chunk.css"
+  },
+  {
+    "revision": "f021d02a8fde673880e4",
+    "url": "/static/js/2.b343dc19.chunk.js"
+  },
+  {
+    "revision": "32d4058f7d1b9638c4e8",
+    "url": "/static/js/3.2be72ac6.chunk.js"
+  },
+  {
+    "revision": "151f6cdde99e468798b2",
+    "url": "/static/js/4.6adbb84d.chunk.js"
+  },
+  {
+    "revision": "9ca26a7d2b4a06e4f44a",
+    "url": "/static/js/main.5d747dcc.chunk.js"
+  },
+  {
+    "revision": "ae9f7de564002b512e19",
+    "url": "/static/js/runtime-main.636d463d.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "fc460399319d2e8bbf493df2b829f44e",
+    "url": "/static/media/CATIA_Logotype.fc460399.png"
+  },
+  {
+    "revision": "b0c603de65e5213ea71e40ffdfc6ffbe",
+    "url": "/static/media/Dawid_copy.b0c603de.jpg"
+  },
+  {
+    "revision": "5a305d7125e486422a74298e6de62408",
+    "url": "/static/media/Matlab_Logo.5a305d71.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "dc199ecc9e90e4bc7e9ed9b31e3c2b5f",
+    "url": "/static/media/Python_Logo.dc199ecc.png"
+  },
+  {
+    "revision": "42b1190b1fa04c2d53fe2618ad938d12",
+    "url": "/static/media/Simulink_logo.42b1190b.png"
+  },
+  {
+    "revision": "f278be7973516129af9e64d45a0c6f4d",
+    "url": "/static/media/c++_Logo.f278be79.png"
+  },
+  {
+    "revision": "1fd881c3869e57487832460992ebf742",
+    "url": "/static/media/cbkLogo.1fd881c3.jpg"
+  },
+  {
+    "revision": "014e1e73535867b7810582bbed54106c",
+    "url": "/static/media/dsLogo.014e1e73.png"
+  },
+  {
+    "revision": "c9013cf0b330f17c771245344dc28b92",
+    "url": "/static/media/dynamika_manipulatora1.c9013cf0.gif"
+  },
+  {
+    "revision": "0b5b248d3657a27c3373884b59d40a7f",
+    "url": "/static/media/erc2016.0b5b248d.jpg"
+  },
+  {
+    "revision": "ef69505c11b8564238f5a1775129d0f3",
+    "url": "/static/media/isaeLogo.ef69505c.jpg"
+  },
+  {
+    "revision": "3d380d66f1701ab615ea3c159a610456",
+    "url": "/static/media/java-logo.3d380d66.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "5f1309b955ba0475b5149027d34bbdd7",
+    "url": "/static/media/manipulator_profil.5f1309b9.PNG"
+  },
+  {
+    "revision": "300f9ae9e4d9b2f5ebad9433f676fb6b",
+    "url": "/static/media/msc_logo.300f9ae9.png"
+  },
+  {
+    "revision": "e4bd90b629742a5445a9b5d0b9ec0d57",
+    "url": "/static/media/siemens.e4bd90b6.png"
+  },
+  {
+    "revision": "ed652b94c5d957f1c38fc844e1e06d94",
+    "url": "/static/media/sirius_panel.ed652b94.jpg"
+  },
+  {
+    "revision": "f7123a19ed4c003f263659e89f42fb6d",
+    "url": "/static/media/sirius_render.f7123a19.jpg"
+  },
+  {
+    "revision": "6f7c8437e1cb4afa1a2db29347ed0dc1",
+    "url": "/static/media/sirius_utah.6f7c8437.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "b7d0ce045b9f33a6a47b749e7c23ddfb",
+    "url": "/static/media/solidworks.b7d0ce04.png"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "1698990704b0f4b2d270d136fa3c9f7b",
+    "url": "/static/media/team_photo.16989907.JPG"
+  },
+  {
+    "revision": "a9e974f75e0ab09384974e87638ecd0f",
+    "url": "/static/media/test1_trajektoria3D.a9e974f7.png"
+  },
+  {
+    "revision": "02eda31144639d36cfbf19050384bb94",
+    "url": "/static/media/wutLogo.02eda311.png"
+  },
+  {
+    "revision": "4cd99958766674cbf8849dda71c4a2f9",
+    "url": "/static/media/zdjecie_aresa.4cd99958.JPG"
+  }
+]);
